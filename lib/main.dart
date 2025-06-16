2c7c9044ac6f56c488edd4f86644eeb1e660d374
ValGorovyi <valvetigu@gmail.com>
2025-06-16 15:36:26 +0300

    multiBlocProvider in upper level, bloc on down level, without Builder, add coment

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,37 @@ import 'package:b_l/bloc-f/users-bloc/users-worcker-b.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+//raznie metody obnovleniya ui
+// context .read / .watch / .select
+// more demonstration, may be no worcked all ui logick
+
 void main() {
   runApp(BlocDemo());
 }
 
+// MultBlocProvider vverhy, poluchaem bloci na nizhnem urovne
 class BlocDemo extends StatelessWidget {
   const BlocDemo({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bloc Demo',
-      home: MyBlocWidget(),
+      home: WrapperBuilderWidget(),
     );
+  }
+}
+
+class WrapperBuilderWidget extends StatelessWidget {
+  // WrapperBuilderWidget({key? key}) :super(key: key); ?? question dyha
+  WrapperBuilderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider<CounterBlocWorcker>(
+          create: (context) => CounterBlocWorcker()..add(BlocPlusEvent())),
+      BlocProvider<UserBlocWorcker>(create: (context) => UserBlocWorcker()),
+    ], child: MyBlocWidget());
   }
 }
 
@@ -27,71 +44,73 @@ class MyBlocWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterBloc = CounterBlocWorcker()..add(BlocPlusEvent());
-    final usersBloc = UserBlocWorcker();
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CounterBlocWorcker>(create: (context) => counterBloc),
-        BlocProvider<UserBlocWorcker>(create: (context) => usersBloc)
-      ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Bloc lesson'),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                onPressed: () {
-                  counterBloc.add(BlocPlusEvent());
-                },
-                icon: Icon(Icons.plus_one)),
-            IconButton(
-                onPressed: () {
-                  counterBloc.add(BlocMinusEvent());
-                },
-                icon: Icon(Icons.exposure_neg_1_outlined)),
-            IconButton(
-                onPressed: () {
-                  usersBloc.add(CreateUsEvent(counterBloc.state));
-                },
-                icon: Icon(Icons.person_2)),
-            IconButton(
-                onPressed: () {
-                  usersBloc.add(CreateJobsEvent(counterBloc.state));
-                },
-                icon: Icon(Icons.work_outline))
-          ],
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                BlocBuilder<CounterBlocWorcker, int>(
-                  bloc: counterBloc,
-                  builder: (context, state) {
-                    return Text(
-                      state.toString(),
-                      style: TextStyle(fontSize: 36),
-                    );
-                  },
-                ),
-                BlocBuilder<UserBlocWorcker, UsersStateB>(
-                    builder: (context, usersSt) {
-                  final users = usersSt.users;
-                  final jobs = usersSt.jobs;
-                  return Column(
-                    children: [
-                      if (usersSt.isLoading) CircularProgressIndicator(),
-                      if (users.isNotEmpty)
-                        ...users.map((elem) => Text(elem.name)),
-                      if (jobs.isNotEmpty) 
-                        ...jobs.map((elem) => Text(elem.name)),
-                    ],
+    // final counterBloc = CounterBlocWorcker()..add(BlocPlusEvent());
+    // final usersBloc = UserBlocWorcker();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloc lesson'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+              onPressed: () {
+
+                final counterBloc =
+                    BlocProvider.of<CounterBlocWorcker>(context);
+
+                counterBloc.add(BlocPlusEvent());
+              },
+              icon: Icon(Icons.plus_one)),
+          IconButton(
+              onPressed: () {
+                // poluchit counterBloc cherez blocProvider
+                final counterBloc =
+                    BlocProvider.of<CounterBlocWorcker>(context);
+                counterBloc.add(BlocMinusEvent());
+              },
+              icon: Icon(Icons.exposure_neg_1_outlined)),
+          IconButton(
+              onPressed: () {
+                // usersBloc.add(CreateUsEvent(counterBloc.state));
+              },
+              icon: Icon(Icons.person_2)),
+          IconButton(
+              onPressed: () {
+                // usersBloc.add(CreateJobsEvent(counterBloc.state));
+              },
+              icon: Icon(Icons.work_outline))
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              BlocBuilder<CounterBlocWorcker, int>(
+                // bloc: counterBloc,
+
+                builder: (context, state) {
+                  return Text(
+                    state.toString(),
+                    style: TextStyle(fontSize: 36),
                   );
-                })
-              ],
-            ),
+                },
+              ),
+              BlocBuilder<UserBlocWorcker, UsersStateB>(
+                  builder: (context, usersSt) {
+                final users = usersSt.users;
+                final jobs = usersSt.jobs;
+                return Column(
+                  children: [
+                    if (usersSt.isLoading) CircularProgressIndicator(),
+                    if (users.isNotEmpty)
+                      ...users.map((elem) => Text(elem.name)),
+                    if (jobs.isNotEmpty) ...jobs.map((elem) => Text(elem.name)),
+                  ],
+                );
+              })
+            ],
           ),
         ),
       ),
